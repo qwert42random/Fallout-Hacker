c4211bf2e411cfeaa2c6acbe734f463f48c4ce5a
qwert42random <Cskywalker1@gmail.com>
2022-07-06 15:45:12 +0100

    Updated dynamic memory
    Added fail state- program exits if memory allocation fails.

--- hacker.cpp
+++ hacker.cpp
@@ -3,27 +3,26 @@
 int main() {
 
     std::string word;
+
     std::string *wordList = (std::string*) malloc(sizeof(std::string));
+    // std::string *wordList = new std::string;
+
     std::ifstream inputFile("exampleInput.txt");
     int wordCount = 0;
-
-    // Get first word in file.
-    // getline(inputFile, word);
-    // wordList[0] = word;
-    // std::cout << word << std::endl;
-    // const int wordLength = word.length();
 
     // Iterate through all words and put them into list.
     while (getline(inputFile, word)) {
 
         std::cout << word << std::endl;
-        wordList[wordCount++] = word;
-
         wordList = (std::string*) realloc(wordList, sizeof(std::string) * (wordCount));
+
         if (wordList == NULL) {
             std::cout << "Error" << std::endl;
             free(wordList);
             return 1;
+        } else {
+            wordList[wordCount] = word;
+            wordCount++;
         }
 
     }
@@ -32,6 +31,7 @@ int main() {
         std::cout << wordList[i] << std::endl;
     }
 
+    // delete[] wordList;
     free(wordList);
 
     return 0;
