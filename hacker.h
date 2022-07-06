45e09f9e21c7de4bef1385384074b6d2a4935ca4
qwert42random <Cskywalker1@gmail.com>
2022-07-06 12:30:25 +0100

    Added dynamic memory
    Memory now allocated dynamically to allow words from file to be saved.

--- hacker.h
+++ hacker.h
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
+#include <cstdlib>
 
 void printHello() {
     std::cout << "Hello" << std::endl;
     return;
+}
+
+int getUserInput() {
+
+    int userInput;
+
+    while (true) {
+
+        std::cout << "How many correct?: ";
+        std::cin >> userInput;
+
+        if (std::cin.fail()) {
+
+            std::cout << "Invalid Input" << std::endl;
+
+            // Flush input buffer.
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        } else {
+
+            return userInput;
+
+        }
+    }
 }
 
